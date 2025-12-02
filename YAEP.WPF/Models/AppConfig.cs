62f305bc39711f1dde6ef895d0882b2645af055f
local9 <apjcookie1@hotmail.com>
2025-12-02 20:41:01 +0000

    chore: remove app config, we don't use it

--- YAEP.WPF/Models/AppConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace YAEP.Models
-{
-    public class AppConfig
-    {
-        public string ConfigurationsFolder { get; set; }
-
-        public string AppPropertiesFileName { get; set; }
-    }
-}
